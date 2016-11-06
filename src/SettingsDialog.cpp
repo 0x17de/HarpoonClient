af840f89f1cfb393855ee2bff5221b408a4747c5
Manuel Herrmann <0@0x17.de>
2016-11-06 11:39:11 +0100

    settigs signal prototypes

--- src/SettingsDialog.cpp
+++ src/SettingsDialog.cpp
@@ -15,3 +15,23 @@ SettingsDialog::~SettingsDialog() {
 void SettingsDialog::show() {
     settingsDialog_.show();
 }
+
+void SettingsDialog::serverAdded(Server* server) {
+    // TODO: handle signal
+}
+void SettingsDialog::serverRemoved(Server* server) {
+    // TODO: handle signal
+}
+void SettingsDialog::serverReset(std::list<Server*> server) {
+    // TODO: handle signal
+}
+void SettingsDialog::hostAdded(Host* host) {
+    // TODO: handle signal
+}
+void SettingsDialog::hostRemoved(Host* host) {
+    // TODO: handle signal
+}
+void SettingsDialog::nickModified(Server* server, const std::string& oldNick, const std::string& newNick) {
+    // TODO: handle signal
+}
+
--- src/SettingsDialog.hpp
+++ src/SettingsDialog.hpp
@@ -4,10 +4,12 @@
 
 #include <QObject>
 
+#include "Server.hpp"
 #include "ui_settings.h"
 #include "ui_ircSettings.h"
 
 
+class Host; // will be added in server.hpp
 class SettingsDialog : public QObject {
     Q_OBJECT
 
@@ -22,6 +24,13 @@ public:
     void show();
 
 public Q_SLOTS:
+    void serverAdded(Server* server);
+    void serverRemoved(Server* server);
+    void serverReset(std::list<Server*> server);
+    void hostAdded(Host* host);
+    void hostRemoved(Host* host);
+    void nickModified(Server* server, const std::string& oldNick, const std::string& newNick);
+
 signals:
 
 };
